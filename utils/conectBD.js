4f5a01bdb8fb9b0ebde6800e26e442f57dc9f352
sad-ai12 <loyalcity127272@gmail.com>
2025-12-03 21:04:06 +0600

    Create conectBD.js

---
+++ utils/conectBD.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('MongoDB Connected');
+};
+
+export default connectDB;
