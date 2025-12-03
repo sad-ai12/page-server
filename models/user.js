db5b0b9d5437056f5a60dcafa4a007e1b89f967b
sad-ai12 <loyalcity127272@gmail.com>
2025-12-03 21:01:55 +0600

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  balance: { type: Number, default: 0 },
+  dailyIncome: { type: Number, default: 0 },
+  rank: { type: String, default: 'LV1' },
+  referralCode: { type: String, unique: true },
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
